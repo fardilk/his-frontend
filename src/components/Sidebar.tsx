e2bc44d5c51204b575cc78b9e0c83733b8e994f7
fardilk <fardil.khalidi@gmail.com>
2025-06-14 12:43:10 +0700

    Add Laravel JWT auth example

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,1 +1,20 @@
-isi
+import { NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard">Dashboard</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  )
+}
+
+export default Sidebar
