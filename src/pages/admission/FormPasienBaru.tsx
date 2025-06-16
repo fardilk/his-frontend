f38dfd5417edfa70765d058220411e2485bb4e27
fardilk <fardil.khalidi@gmail.com>
2025-06-16 15:36:12 +0700

    fix type errors in FormPasienBaru

--- src/pages/admission/FormPasienBaru.tsx
+++ src/pages/admission/FormPasienBaru.tsx
@@ -7,35 +7,153 @@ import KontakPanel from '../../components/form-panels/KontakPanel'
 import KontakDaruratPanel from '../../components/form-panels/KontakDaruratPanel'
 import HubunganKeluargaPanel from '../../components/form-panels/HubunganKeluargaPanel'
 
+interface IdentityNumberForm {
+  nationality: string
+  idType: string
+  idNumber: string
+}
+
+interface PatientDataForm {
+  salutation: string
+  fullName: string
+  placeOfBirth: string
+  dateOfBirth: string
+  gender: string
+  maritalStatus: string
+  country: string
+  bloodType: string
+  religion: string
+  language: string
+  ethnicity: string
+  lastEducation: string
+  occupation: string
+}
+
+interface AddressForm {
+  searchRegion: string
+  village: string
+  subDistrict: string
+  city: string
+  province: string
+  regionCode: string
+  rt: string
+  rw: string
+}
+
+interface ContactForm {
+  countryCode: string
+  phoneNumber: string
+  email: string
+}
+
+interface EmergencyContactForm {
+  relationship: string
+  name: string
+  countryCode: string
+  phoneNumber: string
+  email: string
+  address: string
+}
+
+interface FormState {
+  emergency: boolean
+  identityNumber: IdentityNumberForm
+  patientData: PatientDataForm
+  identityAddress: AddressForm
+  sameAsIdentity: boolean
+  domicileAddress: AddressForm
+  contact: ContactForm
+  emergencyContact: EmergencyContactForm
+  familyPatient: string
+}
+
+type SectionKey =
+  | 'identityNumber'
+  | 'patientData'
+  | 'identityAddress'
+  | 'domicileAddress'
+  | 'contact'
+  | 'emergencyContact'
+
+type SimpleKey = 'emergency' | 'sameAsIdentity' | 'familyPatient'
+
+const initialState: FormState = {
+  emergency: false,
+  identityNumber: { nationality: '', idType: '', idNumber: '' },
+  patientData: {
+    salutation: '',
+    fullName: '',
+    placeOfBirth: '',
+    dateOfBirth: '',
+    gender: '',
+    maritalStatus: '',
+    country: '',
+    bloodType: '',
+    religion: '',
+    language: '',
+    ethnicity: '',
+    lastEducation: '',
+    occupation: '',
+  },
+  identityAddress: {
+    searchRegion: '',
+    village: '',
+    subDistrict: '',
+    city: '',
+    province: '',
+    regionCode: '',
+    rt: '',
+    rw: '',
+  },
+  sameAsIdentity: false,
+  domicileAddress: {
+    searchRegion: '',
+    village: '',
+    subDistrict: '',
+    city: '',
+    province: '',
+    regionCode: '',
+    rt: '',
+    rw: '',
+  },
+  contact: { countryCode: '', phoneNumber: '', email: '' },
+  emergencyContact: {
+    relationship: '',
+    name: '',
+    countryCode: '',
+    phoneNumber: '',
+    email: '',
+    address: '',
+  },
+  familyPatient: '',
+}
+
 const FormPasienBaru: React.FC = () => {
-  const [form, setForm] = useState({
-    emergency: false,
-    identityNumber: { nationality: '', idType: '', idNumber: '' },
-    patientData: { salutation: '', fullName: '', placeOfBirth: '', dateOfBirth: '', gender: '', maritalStatus: '', country: '', bloodType: '', religion: '', language: '', ethnicity: '', lastEducation: '', occupation: '' },
-    identityAddress: { searchRegion: '', village: '', subDistrict: '', city: '', province: '', regionCode: '', rt: '', rw: '' },
-    sameAsIdentity: false,
-    domicileAddress: { searchRegion: '', village: '', subDistrict: '', city: '', province: '', regionCode: '', rt: '', rw: '' },
-    contact: { countryCode: '', phoneNumber: '', email: '' },
-    emergencyContact: { relationship: '', name: '', countryCode: '', phoneNumber: '', email: '', address: '' },
-    familyPatient: ''
-  })
+  const [form, setForm] = useState<FormState>(initialState)
 
   const [showPopup, setShowPopup] = useState(false)
 
-  const handleChange = (section: string, field: string, value: any) => {
+  const handleChange = <S extends SectionKey>(
+    section: S,
+    field: keyof FormState[S],
+    value: FormState[S][keyof FormState[S]],
+  ) => {
     setForm(prev => ({
       ...prev,
       [section]: {
         ...prev[section],
-        [field]: value
-      }
+        [field]: value,
+      },
     }))
   }
 
-  const handleSimpleChange = (field: string, value: any) => {
+  const handleSimpleChange = <S extends SimpleKey>(
+    field: S,
+    value: FormState[S],
+  ) => {
     setForm(prev => ({
       ...prev,
-      [field]: value
+      [field]: value,
     }))
   }
 
