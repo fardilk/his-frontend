43b6e1705f79b3b0f223d9d440baa2b4bf37a4f1
sismedikafardil <fardil@sismedika.com>
2025-06-19 02:53:17 +0700

    perbaikan form-pasien

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,39 @@
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-} from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
+} from 'react-router-dom';
 
-import Dashboard from './pages/Dashboard'
-import Schedule from './pages/Schedule'
-import Workforce from './pages/Workforce'
-import ProtectedRoute from './auth/ProtectedRoute'
-import MainLayout from './layout/MainLayout'
-import AdmissionLayout from './pages/admission/AdmissionLayout'
-import DaftarPasien from './pages/admission/DaftarPasien'
-import FormPasienBaru from './pages/admission/FormPasienBaru'
+import LoginPage from './pages/LoginPage';
+import Dashboard from './pages/Dashboard';
+import Schedule from './pages/Schedule';
+import Workforce from './pages/Workforce';
+import ProtectedRoute from './auth/ProtectedRoute';
+import MainLayout from './layout/MainLayout';
+import AdmissionLayout from './pages/admission/AdmissionLayout';
+import DaftarPasien from './pages/admission/DaftarPasien';
+import FormPasienBaru from './pages/admission/FormPasienBaru';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Navigate to="dashboard" replace />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="schedule" element={<Schedule />} />
-            <Route path="workforce" element={<Workforce />} />
-            <Route path="admission" element={<AdmissionLayout />}>
-              <Route index element={<Navigate to="daftar-pasien" replace />} />
-              <Route path="daftar-pasien" element={<DaftarPasien />} />
-              <Route path="form-pasien-baru" element={<FormPasienBaru />} />
-            </Route>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Navigate to="dashboard" replace />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="schedule" element={<Schedule />} />
+          <Route path="workforce" element={<Workforce />} />
+          <Route path="admission" element={<AdmissionLayout />}>
+            <Route index element={<Navigate to="daftar-pasien" replace />} />
+            <Route path="daftar-pasien" element={<DaftarPasien />} />
+            <Route path="form-pasien-baru" element={<FormPasienBaru />} />
           </Route>
         </Route>
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </Router>
-  )
+      </Route>
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
