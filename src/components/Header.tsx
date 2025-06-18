dd26e2a7a74d55353dde3e42fadc8bc5830d9c43
fardilk <fardil.khalidi@gmail.com>
2025-06-18 16:21:13 +0700

    Add basic vitest setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,7 @@
-isi
+const Header: React.FC = () => (
+  <header className="navbar bg-blue-600 text-white p-4">
+    <h1 className="text-lg font-bold">HIS</h1>
+  </header>
+)
+
+export default Header
