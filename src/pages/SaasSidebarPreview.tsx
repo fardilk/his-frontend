50e08d1322eac25137f095bdd63b9f8f2c90d2c1
fardilk <fardil.khalidi@gmail.com>
2025-06-20 10:45:22 +0700

    refactor: merge SaaS sidebar into Sidebar component

---
+++ src/pages/SaasSidebarPreview.tsx
@@ -0,0 +1,9 @@
+import Sidebar from '../components/Sidebar'
+
+const SaasSidebarPreview: React.FC = () => (
+  <div className="bg-gray-50 min-h-screen flex">
+    <Sidebar />
+  </div>
+)
+
+export default SaasSidebarPreview
