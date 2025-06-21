05afb28fa82800928212f28d11fd437bce2747f7
fardilk <fardil.khalidi@gmail.com>
2025-06-21 21:12:47 +0700

    style: add login page css using theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
 /* eslint-env node */
 /** @type {import('tailwindcss').Config} */
-/* eslint-disable-next-line no-undef */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
