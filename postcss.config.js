b0e42d0b132e19bb9070cdf4a07841aebf5f418e
fardilk <fardil.khalidi@gmail.com>
2025-06-21 21:00:15 +0700

    remove debug log from tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 import tailwindcss from '@tailwindcss/postcss';
 import autoprefixer from 'autoprefixer';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
-  plugins: [tailwindcss(), autoprefixer()],
+  plugins: [tailwindcss({ config: join(__dirname, 'tailwind.config.cjs') }), autoprefixer()],
 };
