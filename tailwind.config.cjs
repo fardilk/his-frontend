ba19216a344a2e965fb5a1fff4669ebfcfae7480
fardilk <fardil.khalidi@gmail.com>
2025-06-22 11:52:48 +0700

    Fix Tailwind content glob

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  './src/**/*.{js,ts,jsx,tsx, css}', // ini cover semua file di src, termasuk components + pages
-] ,
+    './src/**/*.{js,ts,jsx,tsx,css}', // cover all src files including components and pages
+  ],
   theme: {
     extend: {},
   },
